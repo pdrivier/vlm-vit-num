d33df6380702c362e2e65abad19125159dfbf920
Sean <trott.sean@gmail.com>
2025-01-22 10:31:19 -0800

    analysis

--- get_cossim.py
+++ get_cossim.py
@@ -37,20 +37,16 @@ def count_parameters(model):
     return total_params
    
 
-<<<<<<< HEAD
 ## TODO: add an if statement to check for imagelist file's existence!
 image_type = "dots"
-=======
 ## TODO: test this!
-image_type = "rectangles" #or "dots"
->>>>>>> 48f98a4e630fab282b20d421847acf3632b6d981
 dpath = "../vlm-vit-num-tmp/data/stimuli/{x}/".format(x=image_type)
 
 metadata = pd.read_csv(os.path.join(dpath,"metadata.csv"))
 
 imagepair_filename = "imagepair-names-" + image_type + ".pkl"
 if os.path.exists(imagepair_filename):
-<<<<<<< HEAD
+
     with open(imagepair_filename, 'rb') as file:
         list_of_imagepair_names = pickle.load(file)
     image_ix_list = [item for sublist in list_of_imagepair_names for item in sublist]
@@ -58,28 +54,19 @@ if os.path.exists(imagepair_filename):
     select_metadata = metadata.iloc[image_ix_list]
 else:
     select_metadata, list_of_imagepair_names = generate_imagepair_list(metadata,dpath)
-=======
-	with open(imagepair_filename, 'rb') as file:
-	    list_of_imagepair_names = pickle.load(file)
-    image_ix_list = [item for sublist in list_of_imagepair_names for item in sublist]
-	image_ix_list = [i.split("_")[1].split(".png")[0] for i in image_ix_list]
-	select_metadata = metadata.iloc[image_ix_list]
-else:
-	select_metadata, list_of_imagepair_names = generate_imagepair_list(metadata,dpath)
 
->>>>>>> 48f98a4e630fab282b20d421847acf3632b6d981
 
 ## Define the hugging face paths for models and corresponding image processors
 MODELS = {
-    # 'clip-vit-base-patch32': ['openai/clip-vit-base-patch32', CLIPModel, CLIPProcessor], 
-    # 'clip-vit-large-patch14': ['openai/clip-vit-large-patch14', CLIPModel, CLIPProcessor], 
-    # 'clip-huge-14': ['laion/CLIP-ViT-H-14-laion2B-s32B-b79K', CLIPModel, CLIPProcessor], 
-    # 'clip-giant': ['laion/CLIP-ViT-g-14-laion2B-s12B-b42K', CLIPModel, CLIPProcessor], 
-    # 'clip-big-giant': ['laion/CLIP-ViT-bigG-14-laion2B-39B-b160k', CLIPModel, CLIPProcessor], 
+    'clip-vit-base-patch32': ['openai/clip-vit-base-patch32', CLIPModel, CLIPProcessor], 
+    'clip-vit-large-patch14': ['openai/clip-vit-large-patch14', CLIPModel, CLIPProcessor], 
+    'clip-huge-14': ['laion/CLIP-ViT-H-14-laion2B-s32B-b79K', CLIPModel, CLIPProcessor], 
+    'clip-giant': ['laion/CLIP-ViT-g-14-laion2B-s12B-b42K', CLIPModel, CLIPProcessor], 
+    'clip-big-giant': ['laion/CLIP-ViT-bigG-14-laion2B-39B-b160k', CLIPModel, CLIPProcessor], 
     ### 'vit-base-patch16-224-in21k':['google/vit-base-patch16-224-in21k',ViTModel, ViTImageProcessor], ### Already ran
-    ## 'vit-huge-patch14-224-in21k': ['google/vit-huge-patch14-224-in21k', ViTModel, ViTImageProcessor],
-    ## 'vit-large-patch16-224-in21k': ['google/vit-large-patch16-224-in21k', ViTModel, ViTImageProcessor],
-    ## 'vit-large-patch32-224-in21k': ['google/vit-large-patch32-224-in21k', ViTModel, ViTImageProcessor]
+    ### 'vit-huge-patch14-224-in21k': ['google/vit-huge-patch14-224-in21k', ViTModel, ViTImageProcessor], ### Already ran
+    ###'vit-large-patch16-224-in21k': ['google/vit-large-patch16-224-in21k', ViTModel, ViTImageProcessor], ### Already ran
+    ### 'vit-large-patch32-224-in21k': ['google/vit-large-patch32-224-in21k', ViTModel, ViTImageProcessor]### Already ran
     }
 
 ## Set up results-gathering variable
